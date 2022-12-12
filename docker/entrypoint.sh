6c7ba884e9deedad459f9e38135651af18b4ab3f
dodaydream <daydream2013@live.com>
2022-12-13 01:57:54 +0800

    feat: php 8.1 support

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# set permission for folder
+chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
+# install composer dependencies
+composer install --no-dev --no-interaction --optimize-autoloader
+
+# optimize laravel
+php artisan optimize:clear
+php artisan optimize
+
+# Update nginx to match worker_processes to no. of cpu's
+procs=$(cat /proc/cpuinfo | grep processor | wc -l)
+sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
+
+# Start supervisord and services
+/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
