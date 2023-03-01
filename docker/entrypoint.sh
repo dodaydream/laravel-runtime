bf8753d5a4bd366241e8169d4eb508624f9ad41f
dodaydream <daydream2013@live.com>
2023-03-01 16:35:59 +0800

    use gosu to execute in www-data user

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 # install composer dependencies (skip if vendor folder exists)
-if [ ! -d "vendor" ]; then
-  composer install --no-dev --no-interaction --optimize-autoloader
-fi
-
-# Optimize laravel
-# @see: https://laravel.com/docs/9.x/deployment#optimization
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+composer install --no-dev --no-interaction --optimize-autoloader
 
 # Change ownership for folder
 chown -R www-data:www-data /var/www/html
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage
+
+# Optimize laravel
+# @see: https://laravel.com/docs/9.x/deployment#optimization
+# run the follows in www-data user
+gosu www-data:www-data php artisan optimize
 
 # Update nginx to match worker_processes to no. of cpu's
 procs=$(cat /proc/cpuinfo | grep processor | wc -l)
 sed -i -e "s/worker_processes  1/worker_processes $procs/" /etc/nginx/nginx.conf
 
 # Start supervisord and services
-/usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+if [ $# -gt 0 ]; then
+  exec gosu www-data "$@"
+else
+  exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+fi
